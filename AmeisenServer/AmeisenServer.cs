bd3f8e146086bb468e24fa4f259b0d45adf4d102
Jnnshschl <jannis@jnns.de>
2018-08-18 12:00:52 +0200

    Switching to a web interface for the server stuff

--- AmeisenServer/AmeisenServer.cs
+++ AmeisenServer/AmeisenServer.cs
@@ -15,7 +15,8 @@ namespace AmeisenServer
         {
             Console.Title = "AmeisenServer";
 
-            string serverRunning = "#+ Starting AmeisenServer [] +#";
+            WebServer webServer = new WebServer(SendResponse, "http://127.0.0.1:80/");
+            string serverRunning = "#+ Starting AmeisenServer [0.0.0.0:80] +#";
             string fancyBar = "";
 
             foreach (char c in serverRunning)
@@ -32,6 +33,12 @@ namespace AmeisenServer
             Console.WriteLine(fancyBar);
             Console.ResetColor();
 
+            webServer.Run();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("+- Webserver started...");
+            Console.ResetColor();
+
             string cmd = "";
             while (!cmd.ToLower().Equals("stop"))
             {
@@ -39,6 +46,11 @@ namespace AmeisenServer
                 if (!cmd.ToLower().Equals("stop"))
                     Console.WriteLine("unknown: " + cmd);
             }
+        }
+
+        private static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>AmeisenBot Web Interface</BODY></HTML>");
         }
     }
 }
---
+++ AmeisenServer/WebServer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace AmeisenServer
+{
+    public class WebServer
+    {
+        public bool IsRunning { get; private set; }
+
+        private readonly HttpListener httpListener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> responseFunction;
+
+        public WebServer(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> responseFunction)
+        {
+            foreach (var s in prefixes)
+                httpListener.Prefixes.Add(s);
+
+            this.responseFunction = responseFunction;
+            httpListener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes) : this(prefixes, method) { }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                IsRunning = true;
+                try
+                {
+                    while (httpListener.IsListening)
+                        ThreadPool.QueueUserWorkItem(c =>
+                        {
+                            HttpListenerContext listenerContext = (HttpListenerContext)c;
+                            try
+                            {
+                                if (listenerContext == null)
+                                    return;
+
+                                string rstr = responseFunction(listenerContext.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                listenerContext.Response.ContentLength64 = buf.Length;
+                                listenerContext.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { }
+                            finally
+                            {
+                                if (listenerContext != null)
+                                    listenerContext.Response.OutputStream.Close();
+                            }
+                        }, httpListener.GetContext());
+                }
+                catch { }
+            });
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+            httpListener.Stop();
+            httpListener.Close();
+        }
+    }
+}
